fb9fc15f5a9b18fe3ab1493eb6db98b4ce832434
stackstar0 <hafizajakkli20579@gmail.com>
2026-08-11 19:05:41 +0530

    fix: add production domains to CORS allowedOrigins whitelist

--- server/index.ts
+++ server/index.ts
@@ -62,6 +62,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:8080",
   "http://127.0.0.1:3000",
+  "https://instanttrustfunds.com",
+  "https://www.instanttrustfunds.com",
 ];
 
 app.use(
